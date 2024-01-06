d4c41df8e7890c376edbf5dab3199aa020f9d271
Reetesh Yadav <reeteshy15@gmailcom>
2024-01-06 13:05:12 +0530

    Added information for skill

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -30,35 +30,16 @@ const mainBody = {
   ],
 };
 
-// ABOUT SECTION
-// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
-//a) your Instagram username
-//      i.e:profilePictureLink:"johnDoe123",
-//b) a link to an hosted image
-//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
-//c) image in "editable-stuff" directory and use require("") to import here,
-//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
-//d) If you do not want any picture to be displayed, just leave it empty :)
-//      i.e: profilePictureLink: "",
-// For Resume either provide link to your resume or import from "editable-stuff" directory
-//     i.e resume: require("../editable-stuff/resume.pdf"),
-//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
-
 const about = {
   show: true,
   heading: "About Me",
   imageLink: require("../editable-stuff/reeteshy.png"),
   imageSize: 375,
   message:
-    "My name is Reetesh Yadav. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
+    "My name is Reetesh Yadav. A skilled React developer with a passion for crafting seamless user experiences and scalable web applications. With 6 of hands-on experience, I specialize in leveraging the power of React.js to build dynamic and responsive interfaces. Proficient in JavaScript (ES6+), HTML5, and CSS3, I excel in state management using tools like Redux, Typescript, Javascript and Other tools",
   resume: "https://docs.google.com/document/d/1Swlcez4xxgkzTfgq7t-YCr6UjB8oF64M/edit?usp=drive_link&ouid=104367892167973918425&rtpof=true&sd=true",
 };
 
-// PROJECTS SECTION
-// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
-//      i.e: reposLength: 0,
-// If you want to display specfic projects, add the repository names,
-//      i.e ["repository-1", "repo-2"]
 const repos = {
   show: true,
   heading: "Recent Projects",
@@ -96,24 +77,24 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
     { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "React", value: 90 },
+    { name: "Typescript", value: 75 },
+    { name: "Redux", value: 85 },
+    { name: "HTML/CSS", value: 65 },
+    { name: "Next Js", value: 55 },
+    { name: "Node", value: 45 },
+    { name: "Express", value: 45 },
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
+    { name: "Creativity", value: 90 },
     { name: "Positivity", value: 75 },
     { name: "Adaptability", value: 85 },
-    { name: "Problem Solving", value: 75 },
-    { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
-    { name: "Creativity", value: 90 },
+    { name: "Git", value: 80 },
+    { name: "VS Code", value: 90 },
+    { name: "Postman", value: 90 },
+    { name: "GitHub", value: 70 },
   ],
 };
 
@@ -122,7 +103,7 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+    "I'm currently looking for full-time Software Engineering  for Web development opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
   email: "reeteshy15@gmail.com",
 };
 
